ccd0074a0a0bac56518e2f33fd4ecb51c91c5b82
Kar <krlosbatdev@gmail.com>
2020-01-07 00:35:17 -0500

    Add list training

--- services/training-api/list.js
+++ services/training-api/list.js
@@ -1,18 +1,26 @@
 import * as dynamoDbLib from "../../libs/dynamodb-lib";
-import { success, failure } from "../../libs/response-lib";
+import {
+  success,
+  failure
+} from "../../libs/response-lib";
 
 export async function main(event, context) {
   const params = {
     TableName: process.env.tableName,
+    IndexName: 'gsi_1',
     // 'KeyConditionExpression' defines the condition for the query
     // - 'userId = :userId': only return items with matching 'userId'
     //   partition key
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be Identity Pool identity id
     //   of the authenticated user
-    KeyConditionExpression: "userId = :userId",
+    KeyConditionExpression: "sk = :levelDay and begins_with(#data,:weekDay)",
+    ExpressionAttributeNames: {
+      "#data": "data"
+    },
     ExpressionAttributeValues: {
-      ":userId": event.requestContext.identity.cognitoIdentityId
+      ":levelDay": "0",
+      ":weekDay": "Monday"
     }
   };
 
@@ -21,6 +29,8 @@ export async function main(event, context) {
     // Return the matching list of items in response body
     return success(result.Items);
   } catch (e) {
-    return failure({ status: false });
+    return failure({
+      status: false
+    });
   }
 }
