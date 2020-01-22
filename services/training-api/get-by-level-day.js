935a603f0d60e8f5a7f3ed9b1a4fea832fc7b8ac
Kar <krlosbatdev@gmail.com>
2020-01-21 23:19:42 -0500

    Add get-by-level-day

---
+++ services/training-api/get-by-level-day.js
@@ -0,0 +1,37 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.tableName,
+    IndexName: 'gsi_1',
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "sk = :levelDay and begins_with(#data,:weekDay)",
+    ExpressionAttributeNames: {
+      "#data": "data"
+    },
+    ExpressionAttributeValues: {
+      ":levelDay": data.level,
+      ":weekDay": data.day
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      // Return the retrieved item
+      return success(result.Item);
+    } else {
+      return failure({ status: false, error: "Item not found." });
+    }
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
